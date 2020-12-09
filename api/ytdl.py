9dff85f27b956bfc10e7c105eb849527a0bf674b
saanuregh <saanu09xs@gmail.com>
2020-12-09 16:17:03 +0530

    updated deps, cache header changes

--- api/ytdl.py
+++ api/ytdl.py
@@ -33,7 +33,7 @@ async def get_info(q: str, f: str = DEFAULT_FORMAT):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         try:
             res = ydl.extract_info(q, download=False)
-            return JSONResponse(res, headers={"Cache-Control": "max-age=2592000, s-maxage=2592000"})
+            return JSONResponse(res, headers={"Cache-Control": "s-maxage=2592000, stale-while-revalidate"})
         except Exception as e:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=repr(e), headers={"Cache-Control": "no-store, max-age=0"})
 
